99abfb66ed9c198779aadd0affed12d8c6423c82
mcnugget660 <conrad@slotfamily.com>
2026-08-17 22:49:03 -0500

    All Instructions

--- include/CustomCPU/Processor.h
+++ include/CustomCPU/Processor.h
@@ -6,18 +6,25 @@
 
 class Processor {
 public:
-    Processor(std::array<int16_t, 641536> memory);
+    Processor(std::array<int16_t, 641536>* memory);
 
     void fetch();
 
     void execute();
-private:
-    std::array<int16_t, 641536> memory;
 
-    // Handlers
-    std::array<void (Processor::*)(), 16> instruction_table1;
-    std::array<void (Processor::*)(), 16> instruction_table2;
-    std::array<void (Processor::*)(), 9> instruction_table3;
+    bool setOut(uint16_t* value);
+
+    void setIn(uint16_t value);
+
+    bool shouldFork();
+
+    bool shouldKill();
+
+    void setUniverse(uint8_t num);
+
+    uint8_t getUniverse();
+private:
+    std::array<int16_t, 641536>* memory;
 
     // Registers
     int16_t gr[16] = {};
@@ -26,21 +33,11 @@ private:
 
     // Invisible values
     uint16_t nxt_PC = 0;
-
-    // Pointers (dont want to rely on loop burried in Processor.cpp)
-    uint16_t* ugr[16] = {(uint16_t*)&gr[0],(uint16_t*)&gr[1],(uint16_t*)&gr[2],(uint16_t*)&gr[3],
-        (uint16_t*)&gr[4],(uint16_t*)&gr[5],(uint16_t*)&gr[6],(uint16_t*)&gr[7],(uint16_t*)&gr[8],
-        (uint16_t*)&gr[9],(uint16_t*)&gr[10],(uint16_t*)&gr[11],(uint16_t*)&gr[12],(uint16_t*)&gr[13],
-        (uint16_t*)&gr[14],(uint16_t*)&gr[15]};
-
-    uint16_t* PC = &sr[0];
-    uint16_t* RETURN = &sr[1];
-    uint16_t* HI = &sr[2];
-    uint16_t* LOW = &sr[3];
-    uint16_t* CLOCK = &sr[4];
-    uint16_t* STATUS = &sr[5];
-    uint16_t* IN = &sr[6];
-    uint16_t* OUT_R = &sr[7];
+    
+    // Handlers
+    static std::array<void (Processor::*)(), 16> instruction_table1;
+    static std::array<void (Processor::*)(), 16> instruction_table2;
+    static std::array<void (Processor::*)(), 9> instruction_table3;
 
 void ROUTE1();
 void ADD();
@@ -56,8 +53,8 @@ void JMPC();
 void AND();
 void OR();
 void XOR();
-void ADDI();
-void ADDIH();
+void ADDC();
+void ADDCH();
 
 
 void ROUTE2();
@@ -83,8 +80,8 @@ void OUT();
 void NOP();
 void KILL();
 void FORK();
-void INV();
-void COMP();
+void NEG();
+void NOT();
 void CEIL();
 };
 
--- src/Processor.cpp
+++ src/Processor.cpp
@@ -1,115 +1,173 @@
 #include "Processor.h"
 
+#include <iostream>
+
+std::array<void (Processor::*)(), 16> Processor::instruction_table1 = {
+        &Processor::ROUTE1,
+        &Processor::ADD,
+        &Processor::MUL,
+        &Processor::MULU,
+        &Processor::DIV,
+        &Processor::DIVU,
+        &Processor::SETL,
+        &Processor::SETR,
+        &Processor::SET,
+        &Processor::SETU,
+        &Processor::JMPC,
+        &Processor::AND,
+        &Processor::OR,
+        &Processor::XOR,
+        &Processor::ADDC,
+        &Processor::ADDCH
+    };
+    
+std::array<void (Processor::*)(), 16> Processor::instruction_table2 = {
+        &Processor::ROUTE2,
+        &Processor::MOVS,
+        &Processor::MOV,
+        &Processor::STAT,
+        &Processor::LOAD,
+        &Processor::STORE,
+        &Processor::JMPE,
+        &Processor::JMPNE,
+        &Processor::JMPG,
+        &Processor::JMPGE,
+        &Processor::SFTLC,
+        &Processor::SFTRC,
+        &Processor::SFTRAC,
+        &Processor::SFTL,
+        &Processor::SFTR,
+        &Processor::SFTRA
+    };
+    
+std::array<void (Processor::*)(), 9> Processor::instruction_table3 = {
+        &Processor::RET,
+        &Processor::JMP,
+        &Processor::OUT,
+        &Processor::NOP,
+        &Processor::KILL,
+        &Processor::FORK,
+        &Processor::NEG,
+        &Processor::NOT,
+        &Processor::CEIL
+    };
+
+
+#define setStatus(bit) STATUS |= (1 << bit)
+#define setStatusTo(bit, value) STATUS = STATUS & (-1 ^ (1 << bit)) | (1 << value);
+
+#define PC sr[0]
+#define RETURN sr[1]
+#define HI sr[2]
+#define LOW sr[3]
+#define CLOCK sr[4]
+#define STATUS sr[5]
+#define IN_R sr[6]
+#define OUT_R sr[7]
+
+#define ugr(x) ((uint16_t) gr[x])
+
+// >> is assumed to be arithmetic on signed values and logical on unsigned ones
+
 #define r1 ((instruction<<4)>>12)
 #define r2 ((instruction<<8)>>12)
 #define r3 ((instruction<<12)>>12)
 
-void Processor::ROUTE1(){((*this).*instruction_table1[(instruction<<4)>>12])();}
-void Processor::ADD(){gr[r1] = gr[r2] + gr[r3];}
-void Processor::MUL(){int32_t temp = ((int32_t) gr[r2]) * gr[r3]; *HI = temp>>16; *LOW = temp; gr[r1] = *LOW;}
-void Processor::MULU(){uint32_t temp = ((uint32_t) *ugr[r2]) * *ugr[r3]; *HI = temp>>16; *LOW = temp; *ugr[r1] = *LOW;}
-void Processor::DIV(){*HI = gr[r2] / gr[r3]; *LOW = gr[r2] % gr[r3]; gr[r1] = *HI;}
-void Processor::DIVU(){*HI = *ugr[r2] / *ugr[r3]; *LOW = *ugr[r2] % *ugr[r3]; *ugr[r1] = *HI;}
-void Processor::SETL(){*ugr[r1] = ((*ugr[r1]<<8)>>8) + (instruction<<8);}
+void Processor::ROUTE1(){(this->*instruction_table2[(instruction<<4)>>12])();}
+void Processor::ADD(){uint32_t temp = gr[r2] + gr[r3]; setStatusTo(0, temp & (1 << 16)); gr[r1] = temp;}
+void Processor::MUL(){int32_t temp = ((int32_t) gr[r2]) * gr[r3]; HI = temp>>16; LOW = temp; gr[r1] = LOW;}
+void Processor::MULU(){uint32_t temp = ((uint32_t) ugr(r2)) * ugr(r3); HI = temp>>16; LOW = temp; gr[r1] = LOW;}
+void Processor::DIV(){HI = gr[r2] / gr[r3]; LOW = gr[r2] % gr[r3]; gr[r1] = HI;}
+void Processor::DIVU(){HI = ugr(r2) / ugr(r3); LOW = ugr(r2) % ugr(r3); gr[r1] = HI;}
+void Processor::SETL(){gr[r1] = ((ugr(r1)<<8)>>8) + (instruction<<8);}
 void Processor::SETR(){gr[r1] = ((gr[r1]>>8)<<8) + ((instruction<<8)>>8);}
 void Processor::SET(){gr[r1] = (int8_t) ((instruction<<8)>>8);}
 void Processor::SETU(){gr[r1] = ((instruction<<8)>>8);}
-void Processor::JMPC(){nxt_PC = (int16_t) *PC +  (((int16_t)(instruction << 4)) >> 4);}
+void Processor::JMPC(){nxt_PC = (int16_t) PC +  (((int16_t)(instruction << 4)) >> 4);}
 void Processor::AND(){gr[r1] = gr[r2] & gr[r3];}
 void Processor::OR(){gr[r1] = gr[r2] | gr[r3];}
 void Processor::XOR(){gr[r1] = gr[r2] ^ gr[r3];}
-void Processor::ADDI(){gr[r1] += (((int16_t)(instruction << 8)) >> 8);}
-void Processor::ADDIH(){gr[r1] += (((int16_t)(instruction << 8)));}
+void Processor::ADDC(){uint32_t temp = gr[r1] + (((int16_t)(instruction << 8)) >> 8);setStatusTo(0,temp & (1 << 16));gr[1] = temp;}
+void Processor::ADDCH(){uint32_t temp = gr[r1] + (((int16_t)(instruction << 8)));setStatusTo(0,temp & (1 << 16));gr[1] = temp;}
 
 #undef r1
 #undef r2
 #define r1 ((instruction<<8)>>12)
 #define r2 ((instruction<<12)>>12)
 
-void Processor::ROUTE2(){((*this).*instruction_table1[(instruction<<8)>>12])();}
-void Processor::MOVS(){}
-void Processor::MOV(){}
-void Processor::STAT(){}
-void Processor::LOAD(){}
-void Processor::STORE(){}
-void Processor::JMPE(){}
-void Processor::JMPNE(){}
-void Processor::JMPG(){}
-void Processor::JMPGE(){}
-void Processor::SFTLC(){}
-void Processor::SFTRC(){}
-void Processor::SFTRAC(){}
-void Processor::SFTL(){}
-void Processor::SFTR(){}
-void Processor::SFTRA(){}
+void Processor::ROUTE2(){(this->*instruction_table3[(instruction<<8)>>12])();}
+void Processor::MOVS(){gr[r1] = sr[((instruction<<13)>>13)];}
+void Processor::MOV(){gr[r1] = gr[r2];}
+void Processor::STAT(){gr[r1] = (STATUS) & (1 << r2);}
+void Processor::LOAD(){gr[r1] = (*memory)[ugr(r2)];}
+void Processor::STORE(){(*memory)[ugr(r2)] = gr[r1];}
+void Processor::JMPE(){RETURN = PC + 1; nxt_PC = gr[r2] ? nxt_PC : ugr(r1);}
+void Processor::JMPNE(){RETURN = PC + 1; nxt_PC = gr[r2] ? ugr(r1) : nxt_PC;}
+void Processor::JMPG(){RETURN = PC + 1; nxt_PC = gr[r2] > 0 ? ugr(r1) : nxt_PC;}
+void Processor::JMPGE(){RETURN = PC + 1; nxt_PC = gr[r2] >= 0 ? ugr(r1) : nxt_PC;}
+void Processor::SFTLC(){gr[r1] <<= r2;}
+void Processor::SFTRC(){gr[r1] = ugr(r1) >> r2;}
+void Processor::SFTRAC(){gr[r1] >>= r2;}
+void Processor::SFTL(){if(ugr(r2) < 16) gr[r1] <<= gr[r2]; else setStatus(1);}
+void Processor::SFTR(){if(ugr(r2) < 16) gr[r1] = ugr(r1) >> r2; else setStatus(1);}
+void Processor::SFTRA(){if(ugr(r2) < 16) gr[r1] >>= gr[r2]; else setStatus(1);}
 
 #undef r1
 #define r1 ((instruction<<12)>>12)
 
-void Processor::RET(){}
-void Processor::JMP(){}
-void Processor::OUT(){}
+void Processor::RET(){nxt_PC = RETURN;}
+void Processor::JMP(){RETURN = PC + 1; nxt_PC = ugr(r1);}
+void Processor::OUT(){OUT_R = gr[r1];setStatus(3);}
 void Processor::NOP(){}
-void Processor::KILL(){}
-void Processor::FORK(){}
-void Processor::INV(){}
-void Processor::COMP(){}
-void Processor::CEIL(){}
-
+void Processor::KILL(){setStatus(5);}
+void Processor::FORK(){setStatus(4);}
+void Processor::NEG(){gr[r1] = -gr[r1];}
+void Processor::NOT(){gr[r1] = ~gr[r1];}
+void Processor::CEIL(){gr[r1] = gr[r1] < 0 ? 0 : gr[r1];}
 
 
 void Processor::fetch() {
-    instruction = memory.at(*PC);
+    instruction = memory->at(PC);
 }
 
 void Processor::execute() {
-    ((*this).*instruction_table1[instruction>>12])();
+    nxt_PC = PC + 1;
+    (this->*instruction_table1[instruction>>12])();
+    PC = nxt_PC;
+    CLOCK++;
 }
 
-Processor::Processor(std::array<int16_t, 641536> memory) : 
-    memory(memory), instruction_table1{
-        ROUTE1,
-        ADD,
-        MUL,
-        MULU,
-        DIV,
-        DIVU,
-        SETL,
-        SETR,
-        SET,
-        SETU,
-        JMPC,
-        AND,
-        OR,
-        XOR,
-        ADDI,
-        ADDIH
-    }, instruction_table2{
-        ROUTE2,
-        MOVS,
-        MOV,
-        STAT,
-        LOAD,
-        STORE,
-        JMPE,
-        JMPNE,
-        JMPG,
-        JMPGE,
-        SFTLC,
-        SFTRC,
-        SFTRAC,
-        SFTL,
-        SFTR,
-        SFTRA
-    }, instruction_table3{
-        RET,
-        JMP,
-        OUT,
-        NOP,
-        KILL,
-        FORK,
-        INV,
-        COMP,
-        CEIL
-    } {
+
+bool Processor::setOut(uint16_t* value) {
+    if(STATUS ^ (1 << 3))
+        return false;
+    *value = OUT_R;
+    return true;
+}
+
+void Processor::setIn(uint16_t value) {
+    STATUS |= (1 << 2);
+    IN_R = value;
+}
+
+bool Processor::shouldFork() {
+    if(STATUS ^ (1 << 4))
+        return false;
+    setStatusTo(4, 0);
+    return true;
+}
+
+bool Processor::shouldKill() {
+    return(STATUS & (1 << 5));
+}
+
+void Processor::setUniverse(uint8_t num) {
+    STATUS = ((STATUS>>8)<<8) | num;
+}
+
+uint8_t Processor::getUniverse() {
+    return (STATUS<<8)>>8;
+}
+
+Processor::Processor(std::array<int16_t, 641536>* memory) : memory(memory) {
 }
